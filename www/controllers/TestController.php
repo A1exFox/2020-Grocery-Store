88057ae3fb9a57da6c1586ffd2a8194b0679fb06
A1exFox <alex.fox.personal@gmail.com>
2025-06-09 18:34:39 +0300

    #1.04 controllers

---
+++ www/controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+    public $defaultAction = 'my-test';
+
+    public function actionIndex()
+    {
+        return "Hi";
+    }
+
+    public function actionMyTest()
+    {
+        return "Hello";
+    }
+}
