354b47eee9fc199cd6107917742c02f31510d2be
A1exFox <alex.fox.personal@gmail.com>
2025-06-10 13:35:00 +0300

    #1.07 views

--- www/controllers/TestController.php
+++ www/controllers/TestController.php
@@ -7,6 +7,7 @@ use yii\web\Controller;
 class TestController extends Controller
 {
     // public $defaultAction = 'my-test';
+    public $my_var;
 
     // public function actions()
     // {
@@ -17,10 +18,12 @@ class TestController extends Controller
 
     public function actionIndex($name = 'Guest', $age = 25)
     {
+        $this->my_var = 'My variable';
         // return $this->renderFile('@app/views/test/index.php');
         // return $this->renderAjax('index');
         // return $this->renderPartial('index');
         // return $this->render('index', ['name' => $name, 'age' => $age]);
+        \Yii::$app->view->params['t1'] = 'T1 params';
         return $this->render('index', compact('name', 'age'));
     }
 
