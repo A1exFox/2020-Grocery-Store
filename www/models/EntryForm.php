7e9c3196f49255723f140cdfaf6bee0ecfe6dbc4
A1exFox <alex.fox.personal@gmail.com>
2025-06-12 18:05:14 +0300

    #1.13 validation

--- www/models/EntryForm.php
+++ www/models/EntryForm.php
@@ -9,20 +9,36 @@ class EntryForm extends Model
     public $name;
     public $email;
     public $text;
+    public $topic;
 
     public function rules()
     {
         return [
             [['name', 'email', 'text'], 'required'],
             ['email', 'email'],
+            ['topic', 'validateCountry', 'skipOnEmpty' => false],
+            // ['topic', 'string', 'length' => [3, 5]],
+            // ['topic', 'string', 'min' => 3, 'tooShort' => '3 min'],
+            // ['topic', 'string', 'max' => 5, 'tooLong' => '5 max'],
+            // ['topic', 'required', 'message' => 'Oops'],
+            // ['topic', 'trim'],
         ];
     }
+
+    public function validateCountry($attribute, $params)
+    {
+        if (!in_array($this->$attribute, ['USA', 'Indonesia'])) {
+            $this->addError($attribute, "Страна должна быть другой");
+        }
+    }
+
     public function attributeLabels()
     {
         return [
             'name' => 'Имя:',
             'email' => 'E-mail:',
             'text' => 'Текст:',
+            'topic' => 'Тема:',
         ];
     }
 }
--- www/views/test/index.php
+++ www/views/test/index.php
@@ -29,8 +29,9 @@ use yii\widgets\Pjax;
         ],
     ]); ?>
 
-    <?= $form->field($model, 'name')->hint('Заполните поле Имя')->textInput(['placeholder' => 'Введите имя']) ?>
+    <?= $form->field($model, 'name')->textInput(['placeholder' => 'Введите имя']) ?>
     <?= $form->field($model, 'email')->input('email', ['placeholder' => 'Введите email']) ?>
+    <?= $form->field($model, 'topic')->input('text', ['placeholder' => 'Тема сообщения']) ?>
     <?= $form->field($model, 'text')->textarea(['rows' => 7, 'placeholder' => 'Введите текст']) ?>
     <div class="form-group">
         <div class="col-md-5 col-md-offset-2">
