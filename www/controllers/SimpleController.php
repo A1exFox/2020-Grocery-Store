b198d44b6b8851d59408882c6fdac705c456a3e9
A1exFox <alex.fox.personal@gmail.com>
2025-06-19 19:10:47 +0300

    first

---
+++ www/controllers/SimpleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class SimpleController extends Controller
+{
+    public function actionMessage()
+    {
+        return 'First';
+    }
+}
