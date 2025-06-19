0af8a6b3333fcced0ec04714d9d5c3d6ab73d98c
A1exFox <alex.fox.personal@gmail.com>
2025-06-19 19:48:13 +0300

    Revert "override route,action"
    This reverts commit 516fb5c94e5829fbcf4a22a3f3959e7acd5995d6.

--- www/config/web.php
+++ www/config/web.php
@@ -11,7 +11,6 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'defaultRoute' => 'new-route',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
