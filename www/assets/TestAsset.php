e2b332718103bf77170b91b177476ed39b4b7c34
A1exFox <alex.fox.personal@gmail.com>
2025-06-21 18:02:20 +0300

    access from web directory

---
+++ www/assets/TestAsset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class TestAsset extends AssetBundle
+{
+    public $css = ['css/styles.css'];
+
+    public $js = ['js/scripts.js'];
+}
