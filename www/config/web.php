c0aca0da755d36ead8a560a657a154cde60a182e
A1exFox <alex.fox.personal@gmail.com>
2025-06-21 12:30:08 +0300

    clean layout

--- www/config/web.php
+++ www/config/web.php
@@ -11,7 +11,6 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'layout' => 'layout',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
