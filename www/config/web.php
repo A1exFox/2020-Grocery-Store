d16c4ecf83921aa2784269a6e424323ef389fade
A1exFox <alex.fox.personal@gmail.com>
2025-06-22 19:43:26 +0300

    #2.02 set layout grocery store

--- www/config/web.php
+++ www/config/web.php
@@ -11,6 +11,10 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'defaultRoute' => 'home/index',
+    'language' => 'ru',
+    'name' => 'Grocery Store',
+    'layout' => 'grocery',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
