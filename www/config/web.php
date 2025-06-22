0c1d78ff34c64d9e25061e358bde61b08076c732
A1exFox <alex.fox.personal@gmail.com>
2025-06-22 18:19:01 +0300

    #1.27 normalize URL

--- www/config/web.php
+++ www/config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '6KX8eEmQq0AeNvSv054viM3WYepRuBoR',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,14 +43,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
+            'enableStrictParsing' => false,
+            'rules' => [],
         ],
-        */
     ],
     'params' => $params,
 ];
