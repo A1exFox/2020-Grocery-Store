80b691098d896dbe9b27e585aada1518045de1a0
A1exFox <alex.fox.personal@gmail.com>
2025-06-26 13:35:53 +0300

    #2.06 category products page

--- docker/db/grocery_store.sql
+++ docker/db/grocery_store.sql
@@ -88,16 +88,16 @@ LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
 INSERT INTO `product` VALUES
 (1,1,'knorr instant soup (100 gm)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',3.00,0.00,NULL,NULL,'5.png',1),
-(2,1,'chings noodles (75 gm)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',5.00,8.00,NULL,NULL,'6.png',1),
-(3,1,'lahsun sev (150 gm)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',3.00,5.00,NULL,NULL,'7.png',1),
-(4,1,'premium bake rusk (300 gm)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',5.00,7.00,NULL,NULL,'8.png',1),
+(2,1,'chings noodles (75 gm)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',5.00,8.00,NULL,NULL,'6.png',0),
+(3,1,'lahsun sev (150 gm)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',3.00,5.00,NULL,NULL,'7.png',0),
+(4,1,'premium bake rusk (300 gm)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',5.00,7.00,NULL,NULL,'8.png',0),
 (5,4,'fresh spinach (palak)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',2.00,3.00,NULL,NULL,'9.png',1),
-(6,5,'fresh mango dasheri (1 kg)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',5.00,8.00,NULL,NULL,'10.png',1),
+(6,5,'fresh mango dasheri (1 kg)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',5.00,8.00,NULL,NULL,'10.png',0),
 (7,5,'fresh apple red (1 kg)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',6.00,8.00,NULL,NULL,'11.png',1),
-(8,4,'fresh broccoli (500 gm)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',4.00,6.00,NULL,NULL,'12.png',1),
+(8,4,'fresh broccoli (500 gm)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',4.00,6.00,NULL,NULL,'12.png',0),
 (9,10,'mixed fruit juice (1 ltr)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',3.00,0.00,NULL,NULL,'13.png',1),
 (10,10,'prune juice - sunsweet (1 ltr)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',4.00,0.00,NULL,NULL,'14.png',1),
-(11,9,'coco cola zero can (330 ml)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',3.00,0.00,NULL,NULL,'15.png',1),
+(11,9,'coco cola zero can (330 ml)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',3.00,0.00,NULL,NULL,'15.png',0),
 (12,9,'sprite bottle (2 ltr)','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',3.00,0.00,NULL,NULL,'16.png',1);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -111,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2025-06-26  6:14:00
+-- Dump completed on 2025-06-26 10:31:22
